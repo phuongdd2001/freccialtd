a5e3bb23fcfe7a851e3dfe418615c9e108431101
phuong1772001 <phuongdd@volio.vn>
2024-10-08 09:30:16 +0700

    update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,33 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import {
+  PreloadAllModules,
+  provideRouter,
+  withPreloading,
+} from '@angular/router';
+
+import {
+  BrowserModule,
+  provideClientHydration,
+  withHttpTransferCacheOptions,
+} from '@angular/platform-browser';
+import {
+  BrowserAnimationsModule,
+  provideAnimations,
+} from '@angular/platform-browser/animations';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    importProvidersFrom([BrowserModule, BrowserAnimationsModule]),
+    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideClientHydration(
+      withHttpTransferCacheOptions({
+        includePostRequests: true,
+      })
+    ),
+    provideAnimations(),
+    provideHttpClient(withFetch()),
+  ],
 };
