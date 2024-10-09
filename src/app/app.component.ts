372f747b2de6e0ea859a63336da93a82a8fbc516
phuongdd2k1 <phuongdd2001@gmail.com>
2024-10-09 23:25:39 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { TitleComponentComponent } from './components/title-component/title-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink, TitleComponentComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'freccialtd';
+  socials: { icon: string; link: string }[] = [
+    { icon: 'assets/images/ic_facebbook.svg', link: '' },
+    { icon: 'assets/images/ic_instagram.svg', link: '' },
+    { icon: 'assets/images/ic_linkedin.svg', link: '' },
+    { icon: 'assets/images/ic_twitter.svg', link: '' },
+  ];
+  partners: string[] = [
+    'assets/images/ic_partner_1.png',
+    'assets/images/ic_partner_2.png',
+    'assets/images/ic_partner_3.png',
+    'assets/images/ic_partner_4.png',
+  ];
 }
