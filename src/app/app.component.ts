bf1fe9fc62334a30a33f43f7adbc737c5e79105b
phuong1772001 <phuongdd@volio.vn>
2024-10-10 10:20:05 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { TitleComponentComponent } from './components/title-component/title-component.component';
+import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
+import { TitleComponentComponent } from './core/components/title-component/title-component.component';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, TitleComponentComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterLink,
+    TranslateModule,
+    TitleComponentComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  private translate = inject(TranslateService);
+  private route_active = inject(ActivatedRoute);
+
   socials: { icon: string; link: string }[] = [
     { icon: 'assets/images/ic_facebbook.svg', link: '' },
     { icon: 'assets/images/ic_instagram.svg', link: '' },
@@ -23,4 +33,19 @@ export class AppComponent {
     'assets/images/ic_partner_3.png',
     'assets/images/ic_partner_4.png',
   ];
+
+  ngOnInit(): void {
+    this.route_active.url.subscribe((url) => {
+      const lang = url[0]?.path;
+      this.setLanguage(lang);
+    });
+  }
+
+  setLanguage(lang: string) {
+    if (lang && lang === 'vi') {
+      this.translate.setDefaultLang('vi');
+    } else {
+      this.translate.setDefaultLang('en');
+    }
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import {
   PreloadAllModules,
   provideRouter,
+  withDisabledInitialNavigation,
   withPreloading,
 } from '@angular/router';
 
@@ -14,14 +15,33 @@ import {
   BrowserAnimationsModule,
   provideAnimations,
 } from '@angular/platform-browser/animations';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpLoaderFactory } from './core/components/utils/http-loader-factory';
+// import { translateBrowserLoaderFactory } from './core/components/utils/translate-browser.loader';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom([BrowserModule, BrowserAnimationsModule]),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
+    // importProvidersFrom([BrowserModule, BrowserAnimationsModule]),
+    importProvidersFrom([
+      BrowserModule,
+      BrowserAnimationsModule,
+      TranslateModule.forRoot({
+        defaultLanguage: 'en',
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient],
+        },
+      }),
+    ]),
+    provideRouter(
+      routes,
+      withPreloading(PreloadAllModules),
+      withDisabledInitialNavigation()
+    ),
     provideClientHydration(
       withHttpTransferCacheOptions({
         includePostRequests: true,
